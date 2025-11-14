9680aa61ece1249d1fd25b1c5ab7723d088eaec2
Thiago Lima <thiagolima.age@gmail.com>
2025-11-14 20:09:53 -0300

    fix/ estrutura codigo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,10 +6,10 @@ const Home = () =>{
     const [isFocused, setIsFocused] = useState(false);
 
     return (
-        <div className="gradient-dark w-full min-h-screen flex flex-col justify-center items-center">
-            {!isFocused && (
-            <h1 className="text-center text-white text-6xl md:text-8xl">Veja o futuro dos <strong className="text-[var(--roxo)]">profissionais</strong></h1>
-            )}
+        <div className={`gradient-dark w-full min-h-screen flex flex-col justify-center items-center transition-all duration-300 ${isFocused ? 'items-start' : 'items-center'}`}>
+            {// esse h1 so ta desaparecendo prcisa de uma trnasição pra sumir para deixar o input com animação certinha
+            }
+            <h1 className={`text-center text-white text-6xl md:text-8xl ${isFocused ? 'hidden' : null}`}>Veja o futuro dos <strong className="text-[var(--roxo)]">profissionais</strong></h1>
             <Input value={search} onChange={setSearch} onFocus={() => setIsFocused(true)} onBlur={() =>{if (!search) setIsFocused(false)}}/>
         </div>
     )
