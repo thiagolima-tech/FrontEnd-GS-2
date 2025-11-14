126f5a9aa8570dc97cd1394abd59a92a148c36da
Thiago Lima <thiagolima.age@gmail.com>
2025-11-14 19:55:02 -0300

    feat/ funcionalidade basica de usestate no input

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,12 +1,11 @@
 import { useState } from "react"
 
 
-const Input = () =>{
-    const [search, setSearch] = useState('')
+const Input = ({onFocus, onChange, value}) =>{
 
     return (
         <div className="flex w-full justify-center">
-            <input onChange={e => setSearch(e.target.value)} className="border-2 border-white rounded-full my-10 placeholder:text-[#5E5E5E] p-5 w-[90%] md:w-[50%]" type="text" placeholder="Buscar"/>
+            <input value={value} onChange={e => onChange(e.target.value)} onFocus={onFocus} className="border-2 border-white rounded-full my-10 placeholder:text-[#5E5E5E] p-5 w-[90%] md:w-[50%]" type="text" placeholder="Buscar"/>
         </div>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,16 @@
+import { useState } from "react";
 import Input from "../components/Input"
 
 const Home = () =>{
+    const [search, setSearch] = useState('')
+    const [isFocused, setIsFocused] = useState(false);
+
     return (
         <div className="gradient-dark w-full min-h-screen flex flex-col justify-center items-center">
+            {!isFocused && (
             <h1 className="text-center text-white text-6xl md:text-8xl">Veja o futuro dos <strong className="text-[var(--roxo)]">profissionais</strong></h1>
-            <Input/>
+            )}
+            <Input value={search} onChange={setSearch} onFocus={() => setIsFocused(true)} onBlur={() =>{if (!search) setIsFocused(false)}}/>
         </div>
     )
 }
