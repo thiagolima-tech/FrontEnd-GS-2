b73b3b51cf44f1c315ebbc3da2a38d0270994bb4
Vitor Alcantara <vitoralcan5@gmail.com>
2025-11-18 16:02:50 -0300

    feat/ home quase pronta, só falta cards

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,19 +1,39 @@
-import { useState } from "react"
+import React, { useState } from 'react';
+import iconSearch from '../assets/iconSearch.png';
 
 
-const Input = ({onFocus, onBlur, onChange, value}) =>{
+const Input = ({ onFocus, onBlur, onChange, value }) => {
+    const [focused, setFocused] = useState(false);
+
+    const handleFocus = (e) => {
+        setFocused(true);
+        if (typeof onFocus === 'function') onFocus(e);
+    }
+
+    const handleBlur = (e) => {
+        setFocused(false);
+        if (typeof onBlur === 'function') onBlur(e);
+    }
 
     return (
-        <div className="w-full items-center flex justify-center">
-            <input
-                value={value}
-                onChange={e => onChange(e.target.value)}
-                onFocus={onFocus}
-                onBlur={onBlur}
-                className="border-2 border-white rounded-full placeholder:text-[#5E5E5E] mt-10 p-5 w-[70%] transition-all duration-200"
-                type="text"
-                placeholder="Buscar"
-            />
+        <div className="w-full flex justify-center">
+            <div className="relative w-[90%] md:w-[50%] mt-10 mb-5">
+                <img className='absolute left-5 top-1/2 -translate-y-1/2 pointer-events-none' src={iconSearch} alt="icone pesquisar" />
+                <input
+                    value={value}
+                    onChange={e => onChange(e.target.value)}
+                    onFocus={handleFocus}
+                    onBlur={handleBlur}
+                    className={`
+                                border-2 text-white border-white rounded-full text-2xl
+                                placeholder:text-[#5E5E5E] py-3 pl-14 w-full bg-transparent
+                                focus:outline-none focus:ring-0 focus-visible:outline-none
+                                ${focused ? 'drop-shadow-[0_0_40px_#ffffff]' : ''}
+                            `}
+                    type="text"
+                    placeholder="Buscar"
+                />
+            </div>
         </div>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,29 @@
 import { useState } from "react";
 import Input from "../components/Input"
 import lightMode from '../assets/light_mode.png'
+import ProConnectTexto from '../assets/ProConnectTexto.png'
+import ProConnectLogo from '../assets/ProConnectLogo.png'
 
-const Home = () =>{
+const Home = () => {
     const [search, setSearch] = useState('')
     const [isFocused, setIsFocused] = useState(false);
 
     return (
         <div className={`relative gradient-dark w-full min-h-screen flex flex-col justify-center transition-all duration-200 items-center`}>
-            {// esse h1 so ta desaparecendo prcisa de uma trnasição pra sumir para deixar o input com animação certinha
-            }
-            <img className="absolute top-15 right-15" src={lightMode} alt="lightMode" />
+            <img className={`absolute top-5 md:left-50 md:top-15 ${isFocused ? 'opacity-100 translate-y-0' : 'opacity-0 -translate-y-4 scale-75'}`} src={ProConnectTexto} alt="Logotipo Pro Connect" />
+            <img className="absolute top-8 right-8 md:top-15 md:right-15" src={lightMode} alt="lightMode" />
 
             <h1 className={`transform leading-tight w-[311px] md:w-[765px] text-center text-white text-[40px] md:text-8xl transition-all duration-200 ease-in-out ${isFocused ? 'opacity-0 -translate-y-4 scale-75' : 'opacity-100 translate-y-0'}`}>
-                Veja o futuro dos <span className="font-medium" style={{color: 'var(--roxo)'}}>profissionais</span>
+                Veja o futuro dos <span className="font-medium" style={{ color: 'var(--roxo)' }}>profissionais</span>
             </h1>
 
-            <div className={`w-full flex justify-center transition-all duration-300 ease-out ${isFocused ? 'absolute top-15 left-1/2 -translate-x-1/2 md:w-[50%]' : 'relative'}`}>
+            <div className={`w-full flex justify-center transition-all duration-300 ease-out ${isFocused ? 'absolute top-15 md:top-4 left-1/2 -translate-x-1/2' : 'relative'}`}>
                 <div className="w-full">
-                    <Input value={search} onChange={setSearch} onFocus={() => setIsFocused(true)} onBlur={() =>{if (!search) setIsFocused(false)}}/>
-                    <p className={`mt-3 text-[16px] text-white transform transition-all duration-300 ease-out text-center `}>Mostrar todos os resultados</p>
+                    <Input value={search} onChange={setSearch} onFocus={() => setIsFocused(true)} onBlur={() => { if (!search) setIsFocused(false) }} />
+                    <p className={` md:mt-3 text-[16px] text-white transform transition-all duration-300 ease-out text-center `}>Mostrar todos os resultados</p>
                 </div>
             </div>
+            <img className={`absolute bottom-6 mx-auto w-[286px] ${isFocused ? 'opacity-0 -translate-y-4 scale-75' : 'opacity-100 translate-y-0'}`} src={ProConnectLogo} alt="Logotipo Pro Connect"/>
 
         </div>
     )
